a4d7aca64e8c45b934ca12316f43dc8b526407d8
Cindy Liang <hakkacindy@gmail.com>
2021-08-17 23:15:47 +0100

    web skeleton added

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import { Nav } from './Nav';
+
+export const HeroSection = () => (
+  <header>
+    <Nav/>
+
+    <div>
+      <div>
+        <h1>Make smarter decision</h1>
+        <p>Ravelin provides technology and support to help you prevent evolving fraud threats and accept payments
+          with confidence.</p>
+        <button>View solutions</button>
+      </div>
+    </div>
+
+  </header>
+)
---
+++ src/components/SolutionSection.jsx
@@ -0,0 +1,25 @@
+export const SolutionSection = () => (
+  <div>
+    <h2>Learn more about our solutions</h2>
+    <div>
+      <h3>Detect</h3>
+      <p>Understand the risk score for every customer from the moment they land on your website.</p>
+      <a href="#">Learn more</a>
+    </div>
+    <div>
+      <h3>Accept</h3>
+      <p>Manage global regulation and route every payment to the path with the best chance of acceptance.</p>
+      <a href="#">Learn more</a>
+    </div>
+    <div>
+      <h3>Connect</h3>
+      <p>Link analysis to power your investigations, makes machine learning models more effective.</p>
+      <a href="#">Learn more</a>
+    </div>
+    <div>
+      <h3>Protect</h3>
+      <p>Use machine learning models trained on your customer data to quickly detect unusual activity and flag when an account has been hacked.</p>
+      <a href="#">Learn more</a>
+    </div>
+  </div>
+)
