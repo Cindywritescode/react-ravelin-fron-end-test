9d3b8f1f2bd327519b4b2b7345c1dbd8dddafd0b
Cindy Liang <hakkacindy@gmail.com>
2021-08-18 01:56:12 +0100

    add style to navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,17 +1,24 @@
 import { Nav } from './Nav';
+import styled from 'styled-components';
 
 export const HeroSection = () => (
   <header>
     <Nav/>
-
-    <div>
-      <div>
-        <h1>Make smarter decision</h1>
-        <p>Ravelin provides technology and support to help you prevent evolving fraud threats and accept payments
-          with confidence.</p>
-        <button>View solutions</button>
-      </div>
-    </div>
+    <Summary>
+      <h1>Make smarter decision</h1>
+      <p>Ravelin provides technology and support to help you prevent evolving fraud threats and accept payments
+        with confidence.</p>
+      <button>View solutions</button>
+    </Summary>
 
   </header>
-)
+);
+
+const Summary = styled.div`
+  width: 613px;
+  height: 190px;
+  
+  & p{
+    width: 548px;
+  }
+`;
