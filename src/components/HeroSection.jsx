1cf83af4b04e37271d4150e9f41a80cbf0611623
Cindy Liang <hakkacindy@gmail.com>
2021-08-20 17:32:26 +0100

    removed old code from hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -27,7 +27,6 @@ export const Summary = styled.div`
   & p {
     font-size: 1em;
     line-height: 1.5em;
-    //width: 548px;
   }
 
   @media (max-width: 1180px) {
@@ -41,57 +40,3 @@ export const Summary = styled.div`
 const HeroContainer = styled.header`
   height: 50vh;
 `;
-
-// export const BackgroundImg = () => (
-//   <HeroBgImgCol>
-//     <img src="./images/graph-viz.png" alt="product image" width="60%"/>
-//   </HeroBgImgCol>
-// );
-
-// const HeroBgImgCol = styled.div`
-//   position: absolute;
-//   width: 1900px;
-//   height: 600px;
-//   transform: rotate(-15deg);
-//   top: -230px;
-//   left: -120px;
-//   z-index: -1;
-//   background: linear-gradient(90deg, #4E54C8 0%, #8F94FB 100%);
-//   overflow: hidden;
-//
-//
-//   & img {
-//     position: absolute;
-//     top: 180px;
-//     left: 114px;
-//     mix-blend-mode: overlay;
-//     opacity: 0.2;
-//     transform: rotate(14deg);
-//   }
-//
-//   @media (max-width: 1180px) {
-//     transform: rotate(0deg);
-//     top: 0;
-//     left: 0;
-//     height: 65vh;
-//   }
-// `;
-
-// export const ProductImg = () => (
-//   <ProductImgStyle>
-//     <img src="./images/product-fragments.png" alt="product image" width="620px"/>
-//   </ProductImgStyle>
-// );
-//
-//
-// const ProductImgStyle = styled.div`
-//   position: absolute;
-//   top: -12px;
-//   right: -90px;
-//   transform: rotate(-15deg);
-//
-//   @media (max-width: 1180px) {
-//     display: none;
-//   }
-// `;
-
